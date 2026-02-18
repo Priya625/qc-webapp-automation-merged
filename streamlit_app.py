5e7c9484183229f10e994919bd2c0087b6894ed5
PRIYA K V <72011605+Priya625@users.noreply.github.com>
2026-02-18 14:07:33 +0530

    program description changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -857,7 +857,7 @@ with main_qc_tab:
                 """
                 <div class='qc-card'>
                     <h4>10️⃣ Multiple Live Match Consistency Check</h4>
-                    <div class='qc-small'>Flags duplicate entries by flagging rows where the same Live match is recorded multiple times.</div>
+                    <div class='qc-small'>Indentifies duplicate entries by flagging rows where the same Live match is recorded multiple times.</div>
                 </div>
                 """,
                 unsafe_allow_html=True,
